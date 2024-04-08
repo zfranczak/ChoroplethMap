a6fac0462fbf6228f9ab546ad00e31fa8d87b073
Zak Franczak <zakfranczak@gmail.com>
2024-04-08 13:03:04 -0500

    legend made

--- choropleth.js
+++ choropleth.js
@@ -7,15 +7,46 @@ var tooltip = body
   .attr('id', 'tooltip')
   .style('opacity', 0);
 
+var x = d3.scaleLinear().domain([2.6, 75.1]).rangeRound([600, 860]);
+
 var path = d3.geoPath();
 
-var color = d3.scaleThreshold().domain().range(d3.schemePuBuGn[9]);
+var color = d3
+  .scaleThreshold()
+  .domain(d3.range(2.6, 75.1, (75.1 - 2.6) / 8))
+  .range(d3.schemePuBuGn[9]);
 
 var g = svg
   .append('g')
   .attr('class', 'key')
   .attr('id', 'legend')
   .attr('transform', 'translate(0,40)');
+
+g.selectAll('rect')
+  .data(
+    color.range().map(function (d) {
+      d = color.invertExtent(d);
+      if (d[0] === null) {
+        d[0] = x.domain()[0];
+      }
+      if (d[1] === null) {
+        d[1] = x.domain()[1];
+      }
+      return d;
+    })
+  )
+  .enter()
+  .append('rect')
+  .attr('height', 8)
+  .attr('x', function (d) {
+    return x(d[0]);
+  })
+  .attr('width', function (d) {
+    return d[0] && d[1] ? x(d[1]) - x(d[0]) : x(null);
+  })
+  .attr('fill', function (d) {
+    return color(d[0]);
+  });
 
 const countyData =
   'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
