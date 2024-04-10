d52910b56ad7c0d0ba00f67e8005740dd90560a2
Zak Franczak <zakfranczak@gmail.com>
2024-04-09 20:06:43 -0500

    county data incorporated

--- choropleth.js
+++ choropleth.js
@@ -7,6 +7,7 @@ var tooltip = body
   .attr('id', 'tooltip')
   .style('opacity', 0);
 
+//Legend
 var x = d3.scaleLinear().domain([2.6, 75.1]).rangeRound([600, 860]);
 
 var path = d3.geoPath();
@@ -47,6 +48,25 @@ g.selectAll('rect')
   .attr('fill', function (d) {
     return color(d[0]);
   });
+g.append('text')
+  .attr('class', 'caption')
+  .attr('x', x.range()[0])
+  .attr('y', -6)
+  .attr('fill', '#000')
+  .attr('text-anchor', 'start')
+  .attr('font-weight', 'bold');
+
+g.call(
+  d3
+    .axisBottom(x)
+    .tickSize(13)
+    .tickFormat(function (x) {
+      return Math.round(x) + '%';
+    })
+    .tickValues(color.domain())
+)
+  .select('.domain')
+  .remove();
 
 const countyData =
   'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
@@ -55,7 +75,59 @@ const educationData =
 
 Promise.all([d3.json(countyData), d3.json(educationData)])
   .then((data) => {
-    console.log(data[0]); // This will log county data
-    console.log(data[1]); // This will log education data
+    const county = data[0];
+    const education = data[1];
+
+    svg
+      .append('g')
+      .attr('class', 'counties')
+      .selectAll('path')
+      .data(topojson.feature(county, county.objects.counties).features)
+      .enter()
+      .append('path')
+      .attr('class', 'county')
+      .attr('d', path)
+      .attr('fill', (d) => {
+        // Retrieve education data for the county
+        const countyId = d.id;
+        const countyEducation = education.find((e) => e.fips === countyId);
+        return color(countyEducation.bachelorsOrHigher);
+      })
+      .on('mouseover', function (event, d) {
+        // Show tooltip
+        tooltip.transition().duration(200).style('opacity', 0.9);
+        tooltip
+          .html(function () {
+            var result = education.filter(function (obj) {
+              return obj.fips === d.id;
+            });
+            if (result[0]) {
+              return (
+                result[0]['area_name'] +
+                ', ' +
+                result[0]['state'] +
+                ': ' +
+                result[0].bachelorsOrHigher +
+                '%'
+              );
+            }
+
+            return 0;
+          })
+          .attr('data-education', function () {
+            let datapoint = education.filter(function (obj) {
+              return obj.fips === d.id;
+            });
+            if (datapoint[0]) {
+              return datapoint[0].bachelorsOrHigher;
+            }
+          })
+          .style('left', event.pageX + 10 + 'px')
+          .style('top', event.pageY - 28 + 'px');
+      })
+      .on('mouseout', function () {
+        // Hide tooltip
+        tooltip.transition().duration(500).style('opacity', 0);
+      });
   })
   .catch((err) => console.log(err));
